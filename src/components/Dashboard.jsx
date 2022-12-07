ebce0a9623398d0dedf1579545c52e16589fb759
jrobertson610 <119471197+jrobertson610@users.noreply.github.com>
2022-12-07 18:34:31 -0500

    Added video, fixed styling, adjusted Hero image dimensions

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import InventoryTableWrap from './inventorydb/InventoryTableWrap';
-import ContactUsTableWrap from './contactusdb/ContactUsTableWrap';
-import DonationTableWrap from './donationdb/DonationTableWrap';
-import VolunteerTableWrap from './volunteerdb/VolunteerTableWrap';
+import React, { useState, useEffect } from "react";
+import InventoryTableWrap from "./inventorydb/InventoryTableWrap";
+import ContactUsTableWrap from "./contactusdb/ContactUsTableWrap";
+import DonationTableWrap from "./donationdb/DonationTableWrap";
+import VolunteerTableWrap from "./volunteerdb/VolunteerTableWrap";
 
 const Dashboard = () => {
-
   return (
     <div
       name="dashboard"
       className="w-full h-full bg-[#FAEBAD] flex justify-center items-center p-4 pt-[100px]"
     >
       <div>
-        <div className=" max-w-[1200px] mx-auto px-[30px] h-full sm:h-screen">
+        <div className=" max-w-[1200px] mx-auto px-[30px] h-full bg-[#FAEBAD]">
           <div className="sm:text-center pb-8">
-            <h1 className="text-4xl font-bold inline border-b-4 border-[#EB4F31]">
-              
-            </h1>
+            <h1 className="text-4xl font-bold inline border-b-4 border-[#EB4F31]"></h1>
             {/* Container */}
-            <div >
-            <p>This is text from the dashboard</p>
+            <div>
+              <p>This is text from the dashboard</p>
               <ContactUsTableWrap />
               <InventoryTableWrap />
               <DonationTableWrap />
