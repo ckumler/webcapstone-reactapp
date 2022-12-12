82c2916b2dd7f42a0c35258164f68157cd1fa025
Corey <corey.kumler@yahoo.com>
2022-12-12 18:54:56 -0500

    Fix donation table header

--- src/components/donationdb/DonationTable.jsx
+++ src/components/donationdb/DonationTable.jsx
@@ -8,10 +8,10 @@ const DonationTable = ({ donTableData, setDonTableData }) => {
         <thead>
           <tr className="bg-gray-200">
             <th className="px-4 py-2">ID</th>
-            <th className="px-4 py-2">FirstName</th>
-            <th className="px-4 py-2">LastName</th>
             <th className="px-4 py-2">Amount</th>
             <th className="px-4 py-2">Note</th>
+            <th className="px-4 py-2">FirstName</th>
+            <th className="px-4 py-2">LastName</th>
             <th className="px-4 py-2">IsVolunteer</th>
             <th className="px-4 py-2">CreatedAt</th>
             <th className="px-4 py-2">UpdatedAt</th>
